68e5eab3046586d7d0dd30c633734e586bf90b2d
vileven <vileven11@icloud.com>
2017-03-06 01:28:23 +0300

    Refactored code, add idToUser, deleted Anonimus classes, some fix and new methods

---
+++ src/main/java/api/utils/ResponseBody.java
@@ -0,0 +1,15 @@
+package api.utils;
+
+/**
+ * Тело ответа
+ */
+public class ResponseBody {
+    public final int status;
+    public final String msg;
+
+
+    public ResponseBody(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+}
