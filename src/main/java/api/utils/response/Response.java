e4835dbf80eacb1524a3db853e7390d41be22166
vileven <vileven11@icloud.com>
2017-03-10 01:58:45 +0300

    Fixed generic Response

--- src/main/java/api/utils/response/Response.java
+++ src/main/java/api/utils/response/Response.java
@@ -9,44 +9,44 @@ import org.springframework.http.ResponseEntity;
 
 public class Response {
 
-    public static ResponseEntity<ResponseBody> ok(@NotNull String msg) {
+    public static ResponseEntity<? extends ResponseBody> ok(@NotNull String msg) {
         return ResponseEntity.ok(new ResponseBody(ErrorCodes.SUCCESS, msg));
     }
 
-    public static ResponseEntity<ResponseBody> okWithLogin(@NotNull String login, @NotNull String msg) {
+    public static ResponseEntity<? extends ResponseBody> okWithLogin(@NotNull String login, @NotNull String msg) {
         return ResponseEntity.ok(new LoginResponseBody(ErrorCodes.SUCCESS, login, msg));
     }
 
-    public static ResponseEntity<ResponseBody> okWithUser(@NotNull User user, @NotNull String msg) {
+    public static ResponseEntity<? extends ResponseBody> okWithUser(@NotNull User user, @NotNull String msg) {
         return ResponseEntity.ok(new UserResponseBody(ErrorCodes.SUCCESS, user.getLogin(), user.getEmail(),
                 user.getCreatedAt(), user.getUpdatedAt(), msg));
     }
 
-    public static ResponseEntity<ResponseBody> notFound(int status,@NotNull String msg) {
+    public static ResponseEntity<? extends ResponseBody> notFound(int status,@NotNull String msg) {
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseBody(status, msg));
     }
 
-    public static ResponseEntity<ResponseBody> badRequest(int status,@NotNull String msg) {
+    public static ResponseEntity<? extends ResponseBody> badRequest(int status,@NotNull String msg) {
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseBody(status, msg));
     }
 
-    public static ResponseEntity<ResponseBody> userNotFound() {
+    public static ResponseEntity<? extends ResponseBody> userNotFound() {
         return Response.notFound(ErrorCodes.USER_NOT_FOUND, "User not found");
     }
 
-    public static ResponseEntity<ResponseBody> userAlreadyExists() {
+    public static ResponseEntity<? extends ResponseBody> userAlreadyExists() {
         return Response.badRequest(ErrorCodes.USER_ALREADY_EXISTS, "User already exists");
     }
 
-    public static ResponseEntity<ResponseBody> invalidSession() {
+    public static ResponseEntity<? extends ResponseBody> invalidSession() {
         return Response.badRequest(ErrorCodes.SESSION_INVALID, "Invalid session");
     }
 
-    public static ResponseEntity<ResponseBody> badLoginOrPassword() {
+    public static ResponseEntity<? extends ResponseBody> badLoginOrPassword() {
         return Response.badRequest(ErrorCodes.BAD_LOGIN_OR_PASSWORD, "Bad login or password");
     }
 
-    public static ResponseEntity<ResponseBody> badValidator() {
+    public static ResponseEntity<? extends ResponseBody> badValidator() {
         return Response.badRequest(ErrorCodes.BAD_VALIDATOR, "Bad validator");
     }
 
