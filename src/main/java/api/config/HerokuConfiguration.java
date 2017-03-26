b93f5b54ba4e737b5e5369d5e5b6de2ab12ebab5
vileven <vileven11@icloud.com>
2017-03-27 00:46:20 +0300

    fixed bd

--- src/main/java/api/config/HerokuConfiguration.java
+++ src/main/java/api/config/HerokuConfiguration.java
@@ -1,5 +1,6 @@
 package api.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.Profile;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by Vileven on 26.03.17.
@@ -20,19 +24,27 @@ import java.net.URISyntaxException;
 public class HerokuConfiguration {
 
 
-//    @Bean
-//    public DataSource dataSource() throws URISyntaxException {
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+
+//    private static Connection getConnection() throws URISyntaxException, SQLException {
 //        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 //
 //        String username = dbUri.getUserInfo().split(":")[0];
 //        String password = dbUri.getUserInfo().split(":")[1];
 //        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 //
-//        DataSource basicDataSource = new DataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
+//        return DriverManager.getConnection(dbUrl, username, password);
 //    }
 }
