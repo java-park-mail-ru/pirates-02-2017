88c5fec8d0a78494443c33dbaac81b38338af6e5
vileven <vileven11@icloud.com>
2017-03-27 00:55:23 +0300

    fixed bd

---
+++ src/main/java/api/config/Migrations.java
@@ -0,0 +1,16 @@
+package api.config;
+
+import org.flywaydb.core.Flyway;
+
+/**
+ * Created by Vileven on 27.03.17.
+ */
+public class Migrations {
+    public static void main(String[] args) throws Exception {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(System.getenv("JDBC_DATABASE_URL"),
+                System.getenv("JDBC_DATABASE_USERNAME"),
+                System.getenv("JDBC_DATABASE_PASSWORD"));
+        flyway.migrate();
+    }
+}
