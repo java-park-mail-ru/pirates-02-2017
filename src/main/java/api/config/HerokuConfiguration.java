0730cdb5b2551572f745aef89664ea87e9294f96
vileven <vileven11@icloud.com>
2017-03-26 23:44:12 +0300

    started to changed db

--- src/main/java/api/config/HerokuConfiguration.java
+++ src/main/java/api/config/HerokuConfiguration.java
@@ -1,34 +1,34 @@
-package api.config;
-
-import org.hibernate.SessionFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//package api.config;
+//
+//import org.hibernate.SessionFactory;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+////import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 //import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-
-import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-/**
- * Created by Vileven on 26.03.17.
- */
-@Profile("heroku")
-@Configuration
-@EnableAutoConfiguration
-public class HerokuConfiguration {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-//    public SessionFactory sessionFactory() {
-//        SessionFactory.
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.Profile;
+//
+//import javax.sql.DataSource;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//
+///**
+// * Created by Vileven on 26.03.17.
+// */
+//@Profile("heroku")
+//@Configuration
+//@EnableAutoConfiguration
+//public class HerokuConfiguration {
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create().build();
 //    }
-}
+//
+////    public SessionFactory sessionFactory() {
+////        SessionFactory.
+////    }
+//}
