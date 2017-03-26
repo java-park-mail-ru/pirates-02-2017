4bcc65070fffa277c7672076043630cb57f297f3
vileven <vileven11@icloud.com>
2017-03-26 21:39:17 +0300

    fixed heroku

---
+++ src/main/java/api/config/HerokuConfiguration.java
@@ -0,0 +1,32 @@
+package api.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by Vileven on 26.03.17.
+ */
+@Profile("heroku")
+@Configuration
+public class HerokuConfiguration {
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        DataSource basicDataSource = new DataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
