2f075193f201e709eff0b761eac9046f603f310d
Kaibito <gpuzzletime@icloud.com>
2017-03-28 23:19:39 +0300

    Added @CrossOrigin to ApplicationController@

--- src/main/java/api/controllers/generic/ApplicationController.java
+++ src/main/java/api/controllers/generic/ApplicationController.java
@@ -2,8 +2,11 @@ package api.controllers.generic;
 
 import api.services.AccountService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
+@CrossOrigin(origins = {"https://tp314rates.herokuapp.com", "https://project-motion.herokuapp.com",
+        "http://localhost:3000", "*", "http://127.0.0.1:3000"})
 public abstract class ApplicationController {
 
     protected final AccountService accountService;
